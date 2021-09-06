3249b7ba6694124e92ff9e4e8840aa133b9887de
Kundeti Chowdary <rkundeti@magsw.com>
2021-09-06 15:42:02 +0530

    Given Path to the user_input.json file

--- MemphisBambooBuild.py
+++ MemphisBambooBuild.py
@@ -207,8 +207,7 @@ def main():
     if not os.path.exists(input_json_file):
         print("IMPORTANT: Please ensure to modify user_input.json as per your "
               "needs prior to running this.")
-        input_json_file = input("Please enter the full path to the input json "
-                                "file (e.g., C:\\user_input.json): ")
+        input_json_file = sys.argv[3] + 'user_input.json'
     f = open(input_json_file)
     run_bamboo_adapter_build(load(f))
 
